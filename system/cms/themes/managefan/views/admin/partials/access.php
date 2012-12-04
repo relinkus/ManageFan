57bac2c4c1e0ae72ac548fc14fb5b995f24a6993
Edoardo Armandi <armaedo@gmail.com>
2012-12-03 23:19:59 -0200

    Start theme

---
+++ system/cms/themes/managefan/views/admin/partials/access.php
@@ -0,0 +1,5 @@
+<ul id="access" class="children-tooltip">
+	
+	<!--<li><?php if ($this->settings->enable_profiles) echo anchor('edit-profile', lang('cp_edit_profile_label')) ?></li>-->
+								
+</ul>
---
+++ system/cms/themes/managefan/views/admin/partials/side-tabs-shortcuts.php
@@ -0,0 +1,5 @@
+<ul id="shortcuts" class="children-tooltip tooltip-right">
+	
+	<li class="current"><?php echo anchor('admin', lang('global:dashboard'), 'class="shortcut-dashboard"');?></li>
+			
+</ul>
